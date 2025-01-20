f39c999f0f3e51ea12b8500b5343017d3415043b
Léo Belzile <belzilel@gmail.com>
2025-01-20 15:02:07 -0500

    Change Gauss notation

--- _targets.R
+++ _targets.R
@@ -38,7 +38,7 @@ list(
   #
   # The main index.qmd page loads xaringan_slides as a target to link it as a dependency
   tar_files(xaringan_files, list.files(here_rel("slides"),
-                                       pattern = "\\.Rmd",
+                                       pattern = "\\.qmd",
                                        full.names = TRUE)),
   tar_target(xaringan_slides,
              render_xaringan(xaringan_files),
